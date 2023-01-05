e9529a2100de12e56dfbc0d3c63b4cdd99f4790d
tobiasjayas <67170836+tobiasjayas@users.noreply.github.com>
2023-01-05 23:44:55 +0700

    feat(homepage): button href

--- src/lib/components/hero/index.js
+++ src/lib/components/hero/index.js
@@ -1,4 +1,10 @@
+import {BsChevronDoubleDown} from "react-icons/bs"
+
 const Hero = () => {
+  const handleClick = () => {
+    window.location.assign('#about');
+  }
+
   return (
     <div className="bg-blue-hero">
       {/* <img src="assets/hero.png" alt="hero" className="w-full relative lg:-top-14" /> */}
@@ -18,13 +24,20 @@ const Hero = () => {
             backgroundSize: '100%',
           }}
         >
-          <div className="px-6 text-white max-w-md">
-            <h1>Discover Your Wonder</h1>
-            <p className="mt-4 text-sm leading-6">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam.
-            </p>
+          <div className=" text-white max-w-md">
+            <div className="px-6">
+              <h1>Discover Your Wonder</h1>
+              <p className="mt-4 text-sm leading-6">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam.
+              </p>
+            </div>
+            <div className="absolute flex w-full justify-center -bottom-4 lg:-bottom-24">
+              <button onClick={handleClick} className="bg-white shadow-xl p-2 rounded-full">
+                <BsChevronDoubleDown size={20} color="black" />
+              </button>
+            </div>
           </div>
         </div>
       </div>
