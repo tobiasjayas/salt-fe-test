756b2756e11f3f8116f36f77671b3424202d1a58
tobiasjayas <67170836+tobiasjayas@users.noreply.github.com>
2023-01-05 10:46:52 +0700

    feat(homepage): hero

--- src/lib/components/hero/index.js
+++ src/lib/components/hero/index.js
@@ -1,9 +1,19 @@
 const Hero = () => {
   return (
-    <div className="min-h-full bg-blue-hero">
-      <img src="assets/hero.png" alt="hero" className="w-full" />
-      <div className="relative -top-14">
-        <img src="assets/bg-hero.png" alt="bg-hero" className="w-full h-full" />
+    <div className="bg-blue-hero">
+      <img src="assets/hero.png" alt="hero" className="w-full relative lg:-top-14" />
+      <div className="relative -top-14 lg:-top-24">
+        <img src="assets/bg-hero.png" alt="bg-hero" className="w-full" />
+        <div className="absolute top-28 px-6 text-white">
+          <h1 className="text-4xl font-medium leading-9">
+            Discover Your Wonder
+          </h1>
+          <p className="mt-4 text-sm leading-6">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam.
+          </p>
+        </div>
       </div>
     </div>
   )
