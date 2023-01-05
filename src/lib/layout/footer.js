4c526e1c0969bcedff357e5fb5b3925b73f34662
tobiasjayas <67170836+tobiasjayas@users.noreply.github.com>
2023-01-05 21:15:31 +0700

    feat(menu): menu responsive

--- src/lib/layout/footer.js
+++ src/lib/layout/footer.js
@@ -1,7 +1,9 @@
 const Footer = () => {
   return (
-    <div className="footer bg-blue-dark px-6 py-8 lg:py-14">
-      <div className="lg: container mx-auto">
+    <div
+      className="footer bg-blue-dark pt-8 lg:pt-14"
+    >
+      <div className="container mx-auto px-6">
         <img className="h-11" src="logo-company-white.png" alt="logo-footer" />
         <div className="w-full lg:flex lg:gap-x-9">
           <div>
@@ -19,7 +21,7 @@ const Footer = () => {
               </p>
             </div>
           </div>
-          <div className="mt-8 flex flex-col gap-y-3">
+          <div className="mt-8 flex flex-col gap-y-3 z-10">
             <a href="/" className="text-white">
               Who We Are
             </a>
--- src/lib/layout/mobileNav.js
+++ src/lib/layout/mobileNav.js
@@ -1,14 +1,58 @@
-import { MdMenu } from 'react-icons/md'
+import { useState } from 'react'
+import { MdMenu, MdClose } from 'react-icons/md'
+import { useMediaQuery } from 'react-responsive'
 
 const MobileNav = () => {
+  const [open, setOpen] = useState(false)
+  const isDesktop = useMediaQuery({ query: `(min-width: 1024px)` })
+  console.log(isDesktop)
+
+  const handleOpen = () => {
+    if (!open) {
+      setOpen(true)
+    } else {
+      setOpen(false)
+    }
+  }
+
   return (
     <div className="mobile-nav flex px-6 py-4 justify-between z-50 relative bg-white items-center">
       <a href="/">
         <img className="h-8" src="logo-company.png" alt="logo" />
       </a>
-      <span className="flex items-center">
-        <MdMenu size={24} />
-      </span>
+      {!isDesktop && (
+        <>
+          <button onClick={handleOpen} className="flex items-center">
+            {open ? <MdClose size={24} /> : <MdMenu size={24} />}
+          </button>
+        </>
+      )}
+      {open && (
+        <div className="bg-white p-4 border border-gray-300 shadow-xl absolute right-3 top-12 flex flex-col gap-y-6">
+          <a href="/" className="">
+            Who We Are
+          </a>
+          <a href="/" className="">
+            Our Values
+          </a>
+          <a href="/" className="">
+            The Perks
+          </a>
+        </div>
+      )}
+      {isDesktop && (
+        <div className="flex gap-x-8">
+          <a href="/" className="">
+            Who We Are
+          </a>
+          <a href="/" className="">
+            Our Values
+          </a>
+          <a href="/" className="">
+            The Perks
+          </a>
+        </div>
+      )}
     </div>
   )
 }
